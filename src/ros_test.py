6e6c5456bd85edb441231c5df74b4dd6f4dda6e7
Carl Mueller <cmuell89@gmail.com>
2020-06-11 13:45:54 -0700

    Testing ros usage.

--- src/ros_test.py
+++ src/ros_test.py
@@ -12,21 +12,26 @@ from cairo_simulator.log import Logger
 
 
 def main():
-    logger = Logger(handlers=['std', 'logging'], level='debug')
-    logger.info("INFO")
-    logger.debug("DEBUG")
-    logger.warn("WARN")
-    logger.err("ERROR")
-    logger.crit("CRIT")
-    # use_real_time = True
+    if os.environ.get('ROS_DISTRO'):
+        rospy.init_node("CAIRO_Sawyer_Simulator")
+        use_ros = True
+        logger = Logger(handlers=['ros'], level='debug')
+        logger.info("INFO")
+        logger.debug("DEBUG")
+        logger.warn("WARN")
+        logger.err("ERROR")
+        logger.crit("CRIT")
+    else:
+        use_ros = False
+    use_real_time = False
 
-    # sim = Simulator() # Initialize the Simulator
-    # ground_plane = SimObject("Ground", "plane.urdf", [0,0,0])
-    # sawyer_robot = Sawyer("sawyer0", [0, 0, 0.8])
+    sim = Simulator(logger=logger, use_ros=use_ros, use_real_time=use_real_time) # Initialize the Simulator
+    ground_plane = SimObject("Ground", "plane.urdf", [0,0,0])
+    sawyer_robot = Sawyer("sawyer0", [0, 0, 1.0])
 
-    # # Add a table and a Sawyer robot
-    # table = SimObject("Table", ASSETS_PATH + 'table.sdf', (0.9, 0, 0), (0, 0, 1.5708)) # Table rotated 90deg along z-axis
-    # print(p.getNumJoints(table.get_simulator_id()))
+    sim.publish_object_states()
+    sim.publish_robot_states()
+
 
     # sim_obj = SimObject('cube0', 'cube_small.urdf', (0.75, 0, .55))
     # sim_obj = SimObject('cube1', 'cube_small.urdf', (0.74, 0.05, .55))
