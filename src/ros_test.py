692b92332ab633ba85729ff222affc0d7efeac32
Carl Mueller <cmuell89@gmail.com>
2020-06-20 10:57:10 -0700

    More descriptive documentation of choosing between using and not using ROS.

--- src/ros_test.py
+++ src/ros_test.py
@@ -7,46 +7,68 @@ import pybullet as p
 
 from cairo_simulator.simulator import Simulator, SimObject
 from cairo_simulator.manipulators import Sawyer
-from cairo_simulator.utils import ASSETS_PATH
 from cairo_simulator.log import Logger
 
 
 def main():
+    """
+    Illustrates the difference between running within a ROS environment, and running the vanilla Simulator.
+    In sourcing your ROS environment (e.g. source /opt/ros/melodic/setup.bash) certain ROS specific environment variables
+    are set, such as ROS_DISTRO. This could be used to pivot between using ROS and not using ROS. This is really
+    for illustrative purposes mostly. 
+
+    For your own scripts, you could simply write them under the assumption that you will either be using ROS or not, 
+    and the user of your script should be aware of this fact or may run into issues running in either environment.
+    """
     if os.environ.get('ROS_DISTRO'):
-        rospy.init_node("CAIRO_Sawyer_Simulator")
-        use_ros = True
-        logger = Logger(handlers=['ros'], level='debug')
+        # The Logger class is a wrapper around Python's own logging, print(), and rospy logging. See cairo_simulator.log.Logger
+        logger = Logger(handlers=['ros'], level='info')
+        # Awkwardly, the logging level for ROS must be set in the init_node function call. I do not see a workaround as of right now.
+        rospy.init_node("CAIRO_Sawyer_Simulator", log_level=rospy.DEBUG)
+        # Example of a logging statement. There is also .debug() .warn() .crit() .err()
         logger.info("INFO")
-        logger.debug("DEBUG")
-        logger.warn("WARN")
-        logger.err("ERROR")
-        logger.crit("CRIT")
+
+        # If using ROS, make sure to set the use_ros flag in the simulator.
+        use_ros = True
+        use_real_time = False
+
+        # Initialize the Simulator by passing in the logger to be used internally and any appropraite flags.
+        sim = Simulator(logger=logger, use_ros=use_ros, use_real_time=use_real_time)
+        ground_plane = SimObject("Ground", "plane.urdf", [0,0,0])
+        sawyer_robot = Sawyer("sawyer0", [0, 0, 1.0])
+
+        # Try calling ROS specific methods within the Simulator. 
+        try:
+            logger.info("Attempting to execute ROS-based methods.")
+            sim.publish_object_states()
+            sim.publish_robot_states()
+            logger.info("Successfully executed ROS-based methods in the Simulator.")
+        except Exception as e:
+            logger.info("Failed to execute ROS methods within the Simulator.")
+            logger.info(e)
+        time.sleep(1)
+        p.disconnect()
     else:
+        # If running this script without sourcing any ROS environment, this else block section should execute.
+        # Notice we've indicated Python's logger in our Logger class.
+        logger = Logger(handlers=['logging'], level='info')
         use_ros = False
-    use_real_time = False
+        use_real_time = False
+        sim = Simulator(logger=logger, use_ros=use_ros, use_real_time=use_real_time) # Initialize the Simulator
+        ground_plane = SimObject("Ground", "plane.urdf", [0,0,0])
+        sawyer_robot = Sawyer("sawyer0", [0, 0, 1.0])
 
-    sim = Simulator(logger=logger, use_ros=use_ros, use_real_time=use_real_time) # Initialize the Simulator
-    ground_plane = SimObject("Ground", "plane.urdf", [0,0,0])
-    sawyer_robot = Sawyer("sawyer0", [0, 0, 1.0])
+        # This methods should indeed fail and the except block should be reached.
+        try:
+            logger.info("Attempting to execute ROS-based methods.")
+            sim.publish_object_states()
+            sim.publish_robot_states()
+        except Exception as e:
+            logger.info("Failed to execute ROS-based methods within the Simulator.")
+            logger.info(e)
 
-    sim.publish_object_states()
-    sim.publish_robot_states()
-
-
-    # sim_obj = SimObject('cube0', 'cube_small.urdf', (0.75, 0, .55))
-    # sim_obj = SimObject('cube1', 'cube_small.urdf', (0.74, 0.05, .55))
-    # sim_obj = SimObject('cube2', 'cube_small.urdf', (0.67, -0.1, .55))
-    # sim_obj = SimObject('cube3', 'cube_small.urdf', (0.69, 0.1, .55))
-
-    # joint_config = sawyer_robot.solve_inverse_kinematics([0.9,0,1.5], [0,0,0,1])
-    # #sawyer_robot.move_to_joint_pos(joint_config)
-
-    # Loop until someone shuts us down
-    # while rospy.is_shutdown() is not True:
-    #     sim.step()
-    time.sleep(5)
-    p.disconnect()
-
+        time.sleep(1)
+        p.disconnect()
 
 if __name__ == "__main__":
     main()
