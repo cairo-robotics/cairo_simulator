cf5b14ba480a3b57d97883036af4411f1c90a59a
Carl Mueller <cmuell89@gmail.com>
2020-07-12 13:12:39 -0700

    More tinkering.

--- test_planning/test_sawyer_planning.py
+++ test_planning/test_sawyer_planning.py
@@ -78,7 +78,7 @@ def main():
         # Use parametric linear interpolation with 10 steps between points.
         interp = partial(parametric_lerp, steps=10)
         # See params for PRM specific parameters
-        prm = PRM(state_space, svc, interp, params={'n_samples': 6000, 'k': 6, 'ball_radius': 2.0})
+        prm = PRM(state_space, svc, interp, params={'n_samples': 3000, 'k': 6, 'ball_radius': 2.0})
         logger.info("Planning....")
         plan = prm.plan(np.array([0, 0, 0, 0, 0, 0, 0]), np.array([1.5262755737449423, -0.1698540226273928, 2.7788151824762055, 2.4546623466066135, 0.7146948867821279, 2.7671787952787184, 2.606128412644311]))
         # get_path() reuses the interp function to get the path between vertices of a successful plan 
