bc4bb9af9a724475c344e4e41a718ad1f015801d
Carl Mueller <cmuell89@gmail.com>
2020-08-06 16:31:04 -0700

    Loop through simulation steps and show pseudoinverse usage.

--- test_constraints/jacobian_test.py
+++ test_constraints/jacobian_test.py
@@ -50,41 +50,48 @@ def main():
     
     # Get the robot moving!
     sawyer_robot.move_to_joint_pos(target_position=[1]*7)
-    p.stepSimulation()
     
-    # Get current states
-    pos, vel, torq = getJointStates(sawyer_id)
-    names, mpos, mvel, mtorq = getMotorJointStates(sawyer_id)
-    print("Moving joint names:")
-    print(names)
-    
-    sawyerEELink = get_joint_info_by_name(sawyer_id, 'right_l6').idx
-    result = p.getLinkState(sawyer_id,
-                        sawyerEELink,
-                        computeLinkVelocity=1,
-                        computeForwardKinematics=1)
-    link_trn, link_rot, com_trn, com_rot, frame_pos, frame_rot, link_vt, link_vr = result
+    while sawyer_robot.check_if_at_position([1]*7, 0.5) is False:
+        p.stepSimulation()
+        print('\n\n\n\n\n\n')
+        # Get current states
+        pos, vel, torq = getJointStates(sawyer_id)
+        names, mpos, mvel, mtorq = getMotorJointStates(sawyer_id)
+        print("Moving joint names:")
+        print(names)
+        
+        sawyerEELink = get_joint_info_by_name(sawyer_id, 'right_l6').idx
+        result = p.getLinkState(sawyer_id,
+                            sawyerEELink,
+                            computeLinkVelocity=1,
+                            computeForwardKinematics=1)
+        link_trn, link_rot, com_trn, com_rot, frame_pos, frame_rot, link_vt, link_vr = result
 
-    # Get the Jacobians for the CoM of the end-effector link.
-    # Note that in this example com_rot = identity, and we would need to use com_rot.T * com_trn.
-    # The localPosition is always defined in terms of the link frame coordinates.
-    zero_vec = [0.0] * len(mpos)
-    jac_t, jac_r = p.calculateJacobian(sawyer_id, sawyerEELink, com_trn, mpos, zero_vec, zero_vec)
-    J = np.vstack([np.array(jac_t), np.array(jac_r)])[:, [0, 2, 3, 4, 5, 6, 7]]
-    print("JACOBIAN")
-    print(J)
-    J_cross = np.dot(J.T, np.linalg.inv(np.dot(J, J.T)))
-    print("PSEUDOINVERSE JACOBIAN")
-    print(J_cross)
-    
-    test_pose = np.array([1, 1, 1, 0, 0, 0])
-    test_q = np.dot(J_cross, test_pose.T)
-    
-    print("Link linear position and velocity of CoM from getLinkState:")
-    print(link_vt, link_vr)
-    print("Link linear velocity of CoM from linearJacobian * q_dot:")
-    print(np.dot(J, np.array(mvel)[[0, 2, 3, 4, 5, 6, 7]].T))
-
+        # Get the Jacobians for the CoM of the end-effector link.
+        # Note that in this example com_rot = identity, and we would need to use com_rot.T * com_trn.
+        # The localPosition is always defined in terms of the link frame coordinates.
+        zero_vec = [0.0] * len(mpos)
+        jac_t, jac_r = p.calculateJacobian(sawyer_id, sawyerEELink, com_trn, mpos, zero_vec, zero_vec)
+        J = np.vstack([np.array(jac_t), np.array(jac_r)])[:, [0, 2, 3, 4, 5, 6, 7]]
+        print("JACOBIAN")
+        print(J)
+        J_cross = np.dot(J.T, np.linalg.inv(np.dot(J, J.T)))
+        print("PSEUDOINVERSE JACOBIAN")
+        print(J_cross)
+        
+        test_pose = np.array([1, 1, 1, 0, 0, 0])
+        test_q = np.dot(J_cross, test_pose.T)
+        
+        print("Link linear position and velocity of CoM from getLinkState:")
+        print(link_vt, link_vr)
+        print("Link linear andd angular velocity of CoM from linearJacobian * q_dot:")
+        print(np.dot(J, np.array(mvel)[[0, 2, 3, 4, 5, 6, 7]].T))
+        print("Link joint velocities of CoM from getLinkState:")
+        print(np.array(mvel)[[0, 2, 3, 4, 5, 6, 7]])
+        print("Link q given link leanr position and velcotiy using J_cross * x_dot.")
+        x = np.hstack([np.array(link_vt), np.array(link_vr)])
+        q = np.dot(J_cross, np.array(x).T)
+        print(q)
 
 if __name__ == "__main__":
     main()
