6f2039c8b7def0a0cec68d3f11a02f649e6c7b6f
Carl Mueller <cmuell89@gmail.com>
2020-09-09 21:13:45 -0600

    Method to import and repreesnt the LfD model outputs by cairo-lfd

---
+++ src/cairo_planning/lfd/model.py
@@ -0,0 +1,14 @@
+
+
+class KeyframeLFDModel():
+    """
+    Used to represent a learned keyframe LfD model.  
+    """
+    
+    def __init__(self):
+        pass
+
+    def import_model(self, config_file, demonstration_directory):
+        pass
+    
+    
