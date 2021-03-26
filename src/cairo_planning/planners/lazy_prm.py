f600e0535395f25058c7e467accaaf2f9a586ec7
Carl Mueller <cmuell89@gmail.com>
2021-03-26 09:45:35 -0600

    WIP: Lazy constrained PRM for faster planning
    This will move more and more of the planning the sampling portion of planning rather than the CbiRRT2 local planner.

--- src/cairo_planning/planners/lazy_prm.py
+++ src/cairo_planning/planners/lazy_prm.py
@@ -380,3 +380,366 @@ class LazyPRM():
 
     def _value_to_name(self, value):
         return str(["{:.4f}".format(val) for val in value])
+
+
+class LazyCPRM():
+
+    def __init__(self, sim_context_cls, sim_config, robot, tsr, state_space, tree_state_space, state_validity_checker, interpolation_fn, params, tree_params):
+        self.sim_context = sim_context_cls
+        self.sim_config = sim_config
+        self.robot = robot
+        self.tsr = tsr
+        self.state_space = state_space
+        self.tree_state_space = tree_state_space
+        self.svc = state_validity_checker
+        self.interp_fn = interpolation_fn
+        self.n_samples = params.get('n_samples', 600)
+        self.k = params.get('k', 5)
+        self.ball_radius = params.get('ball_radius', .55)
+        self.tree_params = tree_params
+        self.graph = ig.Graph()
+        print("PRM Params: N: {}, k: {}, r: {}".format(
+            self.n_samples, self.k, self.ball_radius))
+
+    def plan(self, q_start, q_goal):
+        # Initial sampling of roadmap and NN data structure.
+        print("Initializing roadmap...")
+        self._init_roadmap(q_start, q_goal)
+        print("Generating valid random samples...")
+        if self.n_samples <= 100:
+            samples = self._generate_samples()
+        else:
+            samples = self._generate_samples_parallel()
+        # Create NN datastructure
+        print("Creating NN datastructure...")
+        self.nn = NearestNeighbors(X=np.array(
+            samples), model_kwargs={"leaf_size": 100})
+        # Generate NN connectivity.
+        print("Generating nearest neighbor connectivity...")
+        if self.n_samples <= 100:
+            self._generate_connections(samples=samples)
+        else:
+            self._generate_connections_parallel(samples=samples)
+        print("Adding connections")
+
+        print("Attaching start and end to graph...")
+        self._attach_start_and_end()
+        print("Finding feasible best path in graph if available...")
+        return self._get_graph_path()
+
+    def get_path(self, plan):
+        points = [self.graph.vs[idx]['value'] for idx in plan]
+        pairs = list(zip(points, points[1:]))
+        segments = [self.interp_fn(np.array(p[0]), np.array(p[1]))
+                    for p in pairs]
+        segments = [[list(val) for val in seg] for seg in segments]
+        path = []
+        for seg in segments:
+            path = path + seg
+        return path
+    
+     def get_lazy_path(self):
+
+        success = False
+        # The successful path we will build by iteratively moving through the best current path and redirecting it as needed according
+        # to state validity etc,.
+        successful_vertex_sequence = []
+        # Find the initial best path in the graph, if it exists.
+        current_best_plan = self._get_best_path(
+            self.start_name, self.goal_name)
+        while not success:
+            if current_best_plan is None or len(current_best_plan) == 1:
+                return []
+
+            invalid_path = True  # we assume an invalid path
+            while invalid_path:
+                # indicator that there was an invalid node. For now we say there are no invalid nodes since hope is all nodes are valid.
+                invalid_node = False
+                # first lets validate points randomly in the path random
+                shuffled_plan = random.sample(
+                    current_best_plan, len(current_best_plan))
+                for point_id in shuffled_plan:
+                    point_value = self.graph.vs[self.graph.vs['id'].index(
+                        point_id)]['value']
+                    if self._validate(point_value):  # validate the point value
+                        # If its valid, set the validity to true to bypass future collision/validity checks.
+                        self.graph.vs.find(self._value_to_name(list(point_value)))[
+                            'validity'] = True
+                    else:
+                        # if the point is invalid, then we remove it and associated edges from the graph
+                         self.graph.delete_vertices(point_id)
+                        invalid_node = True
+                if invalid_node:  # if there was an invalid node, we find a new path and keep the loop going.
+                    current_best_plan = self._get_best_path(
+                        self.start_name, self.goal_name)
+                    if current_best_plan is None or len(current_best_plan) == 1:
+                        return []
+                else:
+                    # since there was no invalid path after looping, then this current_best_path is ready for edge checking.
+                    invalid_path = False
+
+            # let's focus on edges
+            points_traverses = [(_id, self.graph.vs[self.graph.vs['id'].index(_id)]['value'])
+                                for _id in current_best_plan]
+            for idx, _ in enumerate(points_traverses):
+                # we first get the current or 'from' vertex id of wherever we are in the path.
+                from_id = points_traverses[idx][0]
+                # we first get the current or 'from' value of wherever we are in the path.
+                from_value = points_traverses[idx][1]
+                # get the 'to' point vertex id
+                to_id = points_traverses[idx + 1][0]
+                # get the 'to' point value
+                to_value = points_traverses[idx + 1][1]
+                if to_id in successful_vertex_sequence:
+                    continue
+                # validate the 'to' point value, This is probably redundant right now.
+                if self._validate(to_value):
+                    # If its valid, set the validity to true to bypass future collision/validity checks.
+                    self.graph.vs.find(self._val2name(list(to_value)))[
+                        'validity'] = True
+                    # generate an interpolated path between 'from' and 'to'
+                    segment = self.interp_fn(
+                        np.array(from_value), np.array(to_value))
+                    # perform segment evaluation for state validity etc
+                    if self.graph.es[self.graph.get_eid(self.graph.vs['id'].index(from_id), self.graph.vs['id'].index(to_id))].attributes().get('validity', False):
+                        valid = True
+                    else:
+                        valid = subdivision_evaluate(
+                            self.svc.validate, segment)
+                    if valid:
+                        # if valid, we add the 'to' vertex id since we know we can reach it
+                        successful_vertex_sequence.append(to_id)
+                        self.graph.es[self.graph.get_eid(self.graph.vs['id'].index(
+                            from_id), self.graph.vs['id'].index(to_id))]['validity'] = True
+                        goal_idx = self.graph.vs.find(self.goal_name).index
+                        if to_id == goal_idx:
+                            success = True  # if we made a path to goal, then planning was a success and we can break out
+                            break
+                        current_best_plan = self._get_best_path(
+                            self.start_name, self.goal_name)
+                    else:
+                        self._remove_edge(from_id, to_id)
+                        current_best_plan = self._get_best_path(
+                            self.start_name, self.goal_name)
+                        successful_vertex_sequence = []
+                        break  # we break out of looping with for loop to generate a new best path
+                else:
+                    # if the 'to' point is invalid, then we remove it and associated edges from the graph
+                    self.graph.delete_vertices(to_id)
+                    successful_vertex_sequence = []
+                    current_best_plan = self._get_best_path(
+                        self.start_name, self.goal_name)
+                    break
+        successful_vertex_sequence.insert(
+            0, self.graph.vs.find(self.start_name).index)
+        return [self.graph.vs['id'].index(_id) for _id in successful_vertex_sequence]
+
+    def _get_best_path(self, start_name, end_name):
+        graph_idxs = self.graph.get_shortest_paths(
+            start_name, end_name, weights='weight', mode='OUT', output='vpath')[0]
+        best_path = [self.graph.vs[idx]['id'] for idx in graph_idxs]
+        if len(best_path) <= 1:
+            return None
+        else:
+            return best_path
+
+    def _get_graph_path(self):
+        return self.graph.get_shortest_paths(self._name2idx(self.graph, 'start'), self._name2idx(self.graph, 'goal'), weights='weight', mode='ALL')[0]
+
+    def _init_roadmap(self, q_start, q_goal):
+        """
+        Adds the initial vertices of the start and goal configurations.
+
+        Args:
+            q_start (array-like): The starting configuration.
+            q_goal (array-like): The goal configuration.
+        """
+        self.start_name = self._val2name(q_start)
+        self.goal_name = self._val2name(q_goal)
+        self.graph.add_vertex(self.start_name)
+        # Start is always at the 0 index.
+        self.graph.vs[0]["value"] = q_start
+        self.graph.add_vertex(self._val2name(q_goal))
+        # Goal is always at the 1 index.
+        self.graph.vs[1]["value"] = q_goal
+
+    def _attach_start_and_end(self):
+        start = self.graph.vs[self._name2idx(self.graph, 'start')]['value']
+        end = self.graph.vs[self._name2idx(self.graph, 'goal')]['value']
+        start_added = False
+        end_added = False
+        for q_near in self._neighbors(start, k_override=15, within_ball=False):
+            if self._val2name(q_near) in self.graph.vs['name']:
+                if self._val2idx(self.graph, q_near) != 0:
+                    successful = self._cbirrt2_connect(start, q_near)
+                    if successful:
+                        start_added = True
+        for q_near in self._neighbors(end, k_override=15, within_ball=False):
+            if self._val2name(q_near) in self.graph.vs['name']:
+                if self._val2idx(self.graph, q_near) != 1:
+                    successful = self._cbirrt2_connect(q_near, end)
+                    if successful:
+                        end_added = True
+        if not start_added or not end_added:
+            raise Exception("Planning failure! Could not add either start {} and end {} successfully to graph.".format(
+                {start_added}, {end_added}))
+
+    def _generate_samples(self):
+        # sampling_times = [0]
+        count = 0
+        valid_samples = []
+        while count <= self.n_samples:
+            # start_time = timer()
+            q_rand = self._sample()
+            if np.any(q_rand):
+                if self._validate(q_rand):
+                    if count % 100 == 0:
+                        print("{} valid samples...".format(count))
+                    valid_samples.append(q_rand)
+                    count += 1
+                    # sampling_times.append(timer() - start_time)
+            # print(sum(sampling_times) / len(sampling_times))
+        return valid_samples
+
+    def _generate_samples_parallel(self):
+        num_workers = mp.cpu_count()
+        samples_per_worker = int(self.n_samples / num_workers)
+        worker_fn = partial(
+            parallel_projection_worker, sim_context_cls=self.sim_context, sim_config=self.sim_config, tsr=self.tsr)
+        with mp.get_context("spawn").Pool(num_workers) as p:
+            results = p.map(worker_fn, [samples_per_worker] * num_workers)
+            return list(itertools.chain.from_iterable(results))
+
+    def _generate_connections(self, samples):
+        for q_rand in samples:
+            for q_near in self._neighbors(q_rand):
+                _ = self._cbirrt2_connect(q_rand, q_near)
+
+    def _generate_connections_parallel(self, samples):
+        evaluated_name_pairs = []
+        point_pairs = []
+        for q_rand in samples:
+            for q_neighbor in self._neighbors(q_rand):
+                if (self._val2name(q_rand), self._val2name(q_neighbor)) not in evaluated_name_pairs and (self._val2name(q_neighbor), self._val2name(q_rand)) not in evaluated_name_pairs:
+                    point_pairs.append((q_rand, q_neighbor))
+                    evaluated_name_pairs.append(
+                        (self._val2name(q_rand), self._val2name(q_neighbor)))
+                    evaluated_name_pairs.append(
+                        (self._val2name(q_neighbor), self._val2name(q_rand)))
+        num_workers = mp.cpu_count()
+        batches = np.array_split(point_pairs, num_workers)
+        worker_fn = partial(
+            parallel_cbirrt_worker, sim_context_cls=self.sim_context, sim_config=self.sim_config, tsr=self.tsr, tree_state_space=self.tree_state_space, interp_fn=self.interp_fn, tree_params=self.tree_params)
+        with mp.get_context("spawn").Pool(num_workers) as p:
+            batch_results = p.map(worker_fn, batches)
+            # for each set of result, we have a dictionary indexed by the evaluated_name_pairs.
+            # the value of each dictionary is a dictionary with keys 'points' and 'edges'
+            # the points are the actual points and the edges are the edges between points needed between points.
+            # we have to add the points to the graphs, then created edges betweenthem.
+            results = {}
+            for result in batch_results:
+                for named_value_tuple, value in result.items():
+                    results[named_value_tuple] = value
+
+            for named_tuple in evaluated_name_pairs:
+                connection = results.get(named_tuple, None)
+                if connection is not None:
+                    for point in connection['points']:
+                        self._add_vertex(self.graph, point)
+                    for edge in connection['edges']:
+                        self._add_edge(
+                            self.graph, edge[0], edge[1], self._distance(edge[0], edge[1]))
+
+    def _cbirrt2_connect(self, q_near, q_target):
+        centroid = (np.array(q_near) + np.array(q_target)) / 2
+        radius = self._distance(np.array(q_near), np.array(q_target)) / 2
+
+        def _random_config(self):
+            return self.state_space.sample(centroid=centroid, radius=radius)
+
+        # monkey-patch goodness to allow for a special type of sampling via hyperball between, and containing q_near and q_target:
+        CBiRRT2._random_config = _random_config
+
+        cbirrt2 = CBiRRT2(self.robot, self.tree_state_space,
+                          self.svc, self.interp_fn, params=self.tree_params)
+
+        graph_plan = cbirrt2.plan(self.tsr, q_near, q_target)
+        if graph_plan is not None:
+            points = [cbirrt2.connected_tree.vs[idx]['value']
+                      for idx in graph_plan]
+            edges = list(zip(points, points[1:]))
+            for point in points:
+                self._add_vertex(self.graph, point)
+            for e in edges:
+                self._add_edge(self.graph, e[0],
+                               e[1], self._distance(e[0], e[1]))
+            return True
+        else:
+            return False
+
+    def _sample(self):
+        return np.array(self.state_space.sample())
+
+    def _validate(self, sample):
+        return self.svc.validate(sample)
+
+    def _neighbors(self, sample, k_override=None, within_ball=True):
+        if k_override is not None:
+            k = k_override
+        else:
+            k = self.k
+        distances, neighbors = self.nn.query(sample, k=k)
+        if within_ball:
+            return [neighbor for distance, neighbor in zip(
+                distances, neighbors) if distance <= self.ball_radius and distance > 0]
+        else:
+            return [neighbor for distance, neighbor in sorted(
+                list(zip(distances, neighbors)), key=lambda x: x[0]) if distance > 0]
+
+    def _add_vertex(self, graph, q):
+        start_val2name = self._val2name(self.graph.vs[self._name2idx(graph, 'start')]['value'])
+        goal_val2name = self._val2name(self.graph.vs[self._name2idx(graph, 'goal')]['value'])
+        if not self._val2name(q) in graph.vs['name']:
+            if self._val2name(q) != start_val2name and self._val2name(q) != goal_val2name:
+                graph.add_vertex(self._val2name(q), **{'value': q})
+    
+    def _remove_edge(self, graph, q1, q2):
+        start_val2name = self._val2name(self.graph.vs[self._name2idx(graph, 'start')]['value'])
+        goal_val2name = self._val2name(self.graph.vs[self._name2idx(graph, 'goal')]['value'])
+        graph_idx1 = self.graph.vs['id'].index(vid1)
+        graph_idx2 = self.graph.vs['id'].index(vid2)
+        self.graph.delete_edges(self.graph.get_eid(
+            graph_idx1, graph_idx2, directed=False, error=True))
+
+    def _add_edge(self, graph, q_from, q_to, weight):
+        start_val2name = self._val2name(self.graph.vs[self._name2idx(graph, 'start')]['value'])
+        goal_val2name = self._val2name(self.graph.vs[self._name2idx(graph, 'goal')]['value'])
+        if self._val2name(q_from) == start_val2name:
+            q_from_idx = self._name2idx(graph, 'start')
+        elif self._val2name(q_from) == goal_val2name:
+            q_from_idx = self._name2idx(graph, 'goal')
+        else:
+            q_from_idx = self._val2idx(graph, q_from)
+        if self._val2name(q_to) == start_val2name:
+            q_to_idx = self._name2idx(graph, 'start')
+        elif self._val2name(q_to) == goal_val2name:
+            q_to_idx = self._name2idx(graph, 'goal')
+        else:
+            q_to_idx = self._val2idx(graph, q_to)
+        if tuple(sorted([q_from_idx, q_to_idx])) not in set([tuple(sorted(edge.tuple)) for edge in graph.es]):
+            graph.add_edge(q_from_idx, q_to_idx, **{'weight': weight})
+
+    def _delete_vertex(self, graph, value):
+        graph.delete_vertices(self._val2name(value))
+
+    def _val2idx(self, graph, value):
+        return self._name2idx(graph, self._val2name(value))
+
+    def _name2idx(self, graph, name):
+        return graph.vs.find(name).index
+
+    def _val2name(self, value):
+        return str(["{:.4f}".format(val) for val in value])
+
+    def _distance(self, q1, q2):
+        return np.linalg.norm(q1 - q2)
