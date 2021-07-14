3890ecf7a7233e7cb2c1478a8d3d0a5842af604f
carl <cmuell89@gmail.com>
2021-07-13 18:13:15 -0600

    Dumping and loading of model as graphml, json of samples, json of config.

--- src/cairo_planning/core/serialization.py
+++ src/cairo_planning/core/serialization.py
@@ -1,7 +1,7 @@
 import os
 import json
-
-
+import datetime
+import igraph
 
 def dump_PRM(context_config, model, directory_path="./", filename="data.json"):
     """
@@ -11,7 +11,7 @@ def dump_PRM(context_config, model, directory_path="./", filename="data.json"):
     # Dump GraphML 
 
     data = {}
-    data["samples"] = [list(sample) for sample in model.samples]
+    data["samples"] = [[float(val) for val in sample] for sample in model.samples]
     data["config"] = context_config
 
     if not os.path.exists(directory_path):
@@ -25,3 +25,54 @@ def load_PRM(directory_path, filename="data.json"):
     with open(file_path, "r") as file:
         return json.load(file)
 
+
+def dump_model(context_config, model, parent_directory_path="./", directory_name=None):
+    """
+    """
+    # Create a directory if it does not exist
+    # Dump model configuration as JSON
+    # Dump sampples as JSON
+    # Dump GraphML 
+
+    now = datetime.datetime.today()
+    nTime = now.strftime('%Y-%m-%dT%H-%M-%S') if directory_name is None else directory_name
+    directory_path = os.path.join(parent_directory_path, nTime) 
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
+    # Dump Planning Configruration
+    file_path = os.path.join(directory_path, './config.json')
+    with open(file_path, "w") as file:
+        data = {}
+        data["config"] = context_config
+        json.dump(data, file)
+    
+    # Dump Samples
+    file_path = os.path.join(directory_path, './samples.json')
+    with open(file_path, "w") as file:
+        data = {}
+        data["samples"] = [[float(val) for val in sample] for sample in model.samples]
+        json.dump(data, file)
+
+    # Dump Graph as a graph ML
+    file_path = os.path.join(directory_path, './graph.graphml')
+    with open(file_path, "w") as file:
+        model.graph.write_graphml(file)
+
+def load_model(directory_path):
+    # Load Planning Configruration
+    file_path = os.path.join(directory_path, 'config.json')
+    with open(file_path, "r") as file:
+        config = json.load(file)['config']
+    
+    # Load Samples
+    file_path = os.path.join(directory_path, 'samples.json')
+    with open(file_path, "r") as file:
+        samples = json.load(file)['samples']
+
+    # Load Graph as a graph ML
+    file_path = os.path.join(directory_path, 'graph.graphml')
+    with open(file_path, "r") as file:
+        ipython_graph = igraph.Graph.Read_GraphML(file)
+
+    return config, samples, ipython_graph
