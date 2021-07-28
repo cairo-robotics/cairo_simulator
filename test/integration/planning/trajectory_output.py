6715e278424c768097f5c81ae1e7d2feff1393bb
Carl Mueller <cmuell89@gmail.com>
2021-07-28 09:45:39 -0600

    Output basic PRM trajectory to file

---
+++ test/integration/planning/trajectory_output.py
@@ -0,0 +1,70 @@
+import os
+import sys
+from functools import partial
+import time
+import json
+
+import pybullet as p
+if os.environ.get('ROS_DISTRO'):
+    import rospy
+import numpy as np
+
+from cairo_simulator.core.sim_context import SawyerSimContext
+
+from cairo_planning.collisions import DisabledCollisionsContext
+from cairo_planning.local.interpolation import parametric_lerp
+from cairo_planning.local.curve import JointTrajectoryCurve
+from cairo_planning.planners import LazyPRM
+
+
+def main():
+    sim_context = SawyerSimContext()
+    sim = sim_context.get_sim_instance()
+    logger = sim_context.get_logger()
+    state_space = sim_context.get_state_space()
+    svc = sim_context.get_state_validity()
+    sawyer_robot = sim_context.get_robot()
+    _ = sawyer_robot.get_simulator_id()
+    _ = sim_context.get_sim_objects(['Ground'])[0]
+
+    sawyer_robot.move_to_joint_pos([0, 0, 0, 0, 0, 0, 0])
+    time.sleep(1)
+ 
+    with DisabledCollisionsContext(sim, [], []):
+        #######
+        # LazyPRM #
+        #######
+        # Use parametric linear interpolation with 10 steps between points.
+        interp = partial(parametric_lerp, steps=10)
+        # See params for PRM specific parameters
+        prm = LazyPRM(state_space, svc, interp, params={
+                  'n_samples': 4000, 'k': 8, 'ball_radius': 2.5})
+        logger.info("Planning....")
+        plan = prm.plan(np.array([0, 0, 0, 0, 0, 0, 0]), np.array([1.5262755737449423, -0.1698540226273928,
+                                                                   2.7788151824762055, 2.4546623466066135, 0.7146948867821279, 2.7671787952787184, 2.606128412644311]))
+        # get_path() reuses the interp function to get the path between vertices of a successful plan
+        path = prm.get_path(plan)
+    if len(path) == 0:
+        logger.info("Planning failed....")
+        sys.exit(1)
+    logger.info("Plan found....")
+
+    # splinging uses numpy so needs to be converted
+    path = [np.array(p) for p in path]
+    # Create a MinJerk spline trajectory using JointTrajectoryCurve and execute
+    jtc = JointTrajectoryCurve()
+    traj = jtc.generate_trajectory(path, move_time=2)
+    traj_data = {}
+    traj_data["trajectory"] = []
+    for point in traj:
+        traj_point = {
+            "time": point[0],
+            "point": point[1]
+        }
+        traj_data['trajectory'].append(traj_point)
+    with open('traj.json', 'w') as f:
+        json.dump(traj_data, f)
+
+
+if __name__ == "__main__":
+    main()
