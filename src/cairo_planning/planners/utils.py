debaa421a19823d5b5c1f05d26f191407a44a28f
carl <cmuell89@gmail.com>
2021-08-17 16:40:03 -0600

    Moving helper functions into their own module for consistency across planners

---
+++ src/cairo_planning/planners/utils.py
@@ -0,0 +1,28 @@
+from decimal import Decimal, localcontext, ROUND_DOWN
+
+import numpy as np
+
+def name2idx(igraph, name):
+    try:
+        return igraph.vs.find(name).index
+    except Exception as e:
+        pass
+
+def val2str(value, decimal_places=8):
+    def trunc(number, places=8):
+        if not isinstance(places, int):
+            raise ValueError("Decimal places must be an integer.")
+        if places < 1:
+            raise ValueError("Decimal places must be at least 1.")
+        # If you want to truncate to 0 decimal places, just do int(number).
+
+        with localcontext() as context:
+            context.rounding = ROUND_DOWN
+            exponent = Decimal(str(10 ** - places))
+            return Decimal(str(number)).quantize(exponent).to_eng_string()
+    return str([trunc(num, decimal_places) for num in value])
+
+
+def val2idx(igraph, value):
+    return name2idx(igraph, val2str(value))
+
