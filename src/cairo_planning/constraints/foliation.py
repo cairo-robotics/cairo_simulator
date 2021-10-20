09810b91a506a8fbb93d28234862dcf6cd9f0115
Carl <cmuell89@gmail.com>
2021-10-20 13:15:56 -0600

    Foliation classification

---
+++ src/cairo_planning/constraints/foliation.py
@@ -0,0 +1,17 @@
+
+
+class FoliationCluster():
+
+    def __init__():
+        pass
+
+    def fit(X):
+        pass
+
+    def classify(sample):
+        pass
+
+def get_foliation(sample, model):
+    
+    
+    model.
