e071c6419a16f55d527c7f90e008988e432ffd78
Carl <cmuell89@gmail.com>
2021-10-27 14:57:24 -0600

    The most common foliation cluster ID is the winner

--- src/cairo_planning/constraints/foliation.py
+++ src/cairo_planning/constraints/foliation.py
@@ -1,5 +1,8 @@
 from sklearn.mixture import BayesianGaussianMixture
 
+##########
+# MODELS #
+##########
 class VGMMFoliationClustering():
 
     def __init__(self, estimated_foliations=2):
@@ -10,3 +13,12 @@ class VGMMFoliationClustering():
 
     def predict(self, sample):
         return self.model.predict(sample)
+
+####################
+# Helper Functions #
+####################
+def winner_takes_all(X, foliation_model):
+    results = []
+    for x in X:
+        results.append(foliation_model.predict(x))
+    return max(set(results), key = results.count)
