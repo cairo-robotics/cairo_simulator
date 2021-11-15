Wrap to interval doesn't appear to fix jacobian near singularity issues

src/cairo_planning/constraints/projection.py
@@ -94,9 +94,9 @@ def project_config(manipulator, tsr, q_s, q_old, epsilon, q_step=.5, e_step=.25,
         if wrap_to_interval:
             q_s_new = []
             for val in q_s:
-                q_s_new.appen(w2i(val))
-            q_s = q_s_new
-        if np.linalg.norm(q_s - q_old) > 2 * q_step or not within_joint_limits(manipulator, q_s):
+                q_s_new.append(w2i(val))
+            q_s = np.array(q_s_new)
+        if np.linalg.norm(q_s - np.array(q_old)) > 2 * q_step or not within_joint_limits(manipulator, q_s):
                 return None
 
         # if not within_joint_limits(manipulator, q_s):