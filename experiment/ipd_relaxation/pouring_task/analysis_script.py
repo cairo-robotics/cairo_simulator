16c7de400371f788d0dae945c4573247f8daa8fa
Carl Mueller <cmuell89@gmail.com>
2022-10-10 08:05:03 -0600

    Updated analysis script to group participants

--- experiment/ipd_relaxation/pouring_task/analysis_script.py
+++ experiment/ipd_relaxation/pouring_task/analysis_script.py
@@ -7,53 +7,48 @@ FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 if __name__ == "__main__":
-    participants = ["1"]
-    data = {}
-    for participant in participants:
-        INPUT_DIR = os.path.join(FILE_DIR, "participant_{}/output".format(participant))
-        print(INPUT_DIR)
-        data[participant] = import_data_as_dataframe(INPUT_DIR)
+
+    #########################
+    # CONSTANTS / VARIABLES #
+    #########################
+    EVAL_OUTPUT_DIRECTORY = os.path.join(FILE_DIR, "data/output_combined")
+    df = import_data_as_dataframe(EVAL_OUTPUT_DIRECTORY)
     
     #############################
     # Planning Success Analysis #
     #############################
-    for participant, participant_df in data.items():
-        psa = PlanningSuccessAnalysis(participant_df)
-        print(psa.analyze())
+    psa = PlanningSuccessAnalysis(df)
+    print(psa.analyze())
 
     ##########################
     # Planning Time Analysis #
     ##########################
-    for  participant, participant_df in data.items():
-        pta = PlanningTimeAnalysis(participant_df)
-        print(pta.analyze())
+    pta = PlanningTimeAnalysis(df)
+    print(pta.analyze())
     
     ########################
     # Path Length Analysis #
     ########################
-    for  participant, participant_df in data.items():
-        pla = PathLengthAnalysis(participant_df)
-        print(pla.analyze())
+  
+    pla = PathLengthAnalysis(df)
+    print(pla.analyze())
         
     #############################
     # A2S Config Space Analysis #
     #############################
-    for  participant, participant_df in data.items():
-        a2scs = A2SConfigSpaceAnalysis(participant_df)
-        print(a2scs.analyze())
+    a2scs = A2SConfigSpaceAnalysis(df)
+    print(a2scs.analyze())
     
     ###########################
     # A2S Task Space Analysis #
     ###########################
-    for  participant, participant_df in data.items():
-        a2sts = A2STaskSpaceAnalysis(participant_df)
-        print(a2sts.analyze())
+    a2sts = A2STaskSpaceAnalysis(df)
+    print(a2sts.analyze())
     
     ################
     # A2F Analysis #
     ################
-    for  participant, participant_df in data.items():
-        a2f = A2FAnalysis(participant_df)
-        print(a2f.analyze())
+    a2f = A2FAnalysis(df)
+    print(a2f.analyze())
     
     
