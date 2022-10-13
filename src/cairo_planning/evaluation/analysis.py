3e4d885e99508307df469b804e9c73a65b2424f3
Carl <cmuell89@gmail.com>
2022-10-12 18:26:43 -0600

    standard deviation and mean

--- src/cairo_planning/evaluation/analysis.py
+++ src/cairo_planning/evaluation/analysis.py
@@ -56,8 +56,8 @@ class PlanningSuccessAnalysis():
     #     plt.close()
         
     def analyze(self):
-        df = self.dataframe[["participant", "planning_bias", "collision_objects", "ip_style", "success"]]
-        return df.groupby(['participant', 'planning_bias', "collision_objects", 'ip_style']).mean()
+        df = self.dataframe[["planning_bias", "collision_objects", "ip_style", "success"]]
+        return df.groupby(['planning_bias', "collision_objects", 'ip_style']).mean()
 class PlanningTimeAnalysis():
 
     def __init__(self, dataframe):
@@ -76,7 +76,9 @@ class PlanningTimeAnalysis():
     def analyze(self):
         rslt_df = self.dataframe[self.dataframe["planning_time"] > 0]
         df = rslt_df[["participant", "planning_bias", "collision_objects", "ip_style", "planning_time"]]
-        return df.groupby(['participant', 'planning_bias', "collision_objects", 'ip_style']).mean()
+        mean = df.groupby(['planning_bias', "collision_objects", 'ip_style']).mean()
+        std = df.groupby(['planning_bias', "collision_objects", 'ip_style']).std()
+        return mean, std
     
 class PathLengthAnalysis():
 
@@ -96,7 +98,9 @@ class PathLengthAnalysis():
     def analyze(self):
         rslt_df = self.dataframe[self.dataframe["path_length"] > 0]
         df = rslt_df[["participant", "planning_bias", "collision_objects", "ip_style", "path_length"]]
-        return df.groupby(['participant', 'planning_bias', "collision_objects", 'ip_style']).mean()
+        mean = df.groupby(['planning_bias', "collision_objects", 'ip_style']).mean()
+        std = df.groupby(['planning_bias', "collision_objects", 'ip_style']).std()
+        return mean, std
 
 class A2SConfigSpaceAnalysis():
 
@@ -115,7 +119,9 @@ class A2SConfigSpaceAnalysis():
     def analyze(self):
         rslt_df = self.dataframe[self.dataframe["a2s_cspace_distance"] > 0]
         df = rslt_df[["participant", "planning_bias", "collision_objects", "ip_style", "a2s_cspace_distance"]]
-        return df.groupby(['participant', 'planning_bias', "collision_objects", 'ip_style']).mean()
+        mean = df.groupby(['planning_bias', "collision_objects", 'ip_style']).mean()
+        std = df.groupby(['planning_bias', "collision_objects", 'ip_style']).std()
+        return mean, std
     
 class A2STaskSpaceAnalysis():
 
@@ -134,7 +140,9 @@ class A2STaskSpaceAnalysis():
     def analyze(self):
         rslt_df = self.dataframe[self.dataframe["a2s_taskspace_distance"] > 0]
         df = rslt_df[["participant", "planning_bias", "collision_objects", "ip_style", "a2s_taskspace_distance"]]
-        return df.groupby(['participant', 'planning_bias', "collision_objects", 'ip_style']).mean()
+        mean = df.groupby(['planning_bias', "collision_objects", 'ip_style']).mean()
+        std = df.groupby(['planning_bias', "collision_objects", 'ip_style']).std()
+        return mean, std
     
 class A2FAnalysis():
 
@@ -153,5 +161,7 @@ class A2FAnalysis():
     def analyze(self):
         rslt_df = self.dataframe[self.dataframe["a2f_percentage"] > 0]
         df = rslt_df[["participant", "planning_bias", "collision_objects", "ip_style", "a2f_percentage"]]
-        return df.groupby(['participant', 'planning_bias', "collision_objects", 'ip_style']).mean()
+        mean = df.groupby(['planning_bias', "collision_objects", 'ip_style']).mean()
+        std = df.groupby(['planning_bias', "collision_objects", 'ip_style']).std()
+        return mean, std
 
